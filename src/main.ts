5fa503d3ca3f692fe4c004ea12aa62c9764cb463
Gabriel Silveira <bsi.silveira@gmail.com>
2025-03-26 01:16:10 -0300

    feat: :construction: initialize project structure with Vue, Vite, and Cypress setup
    initialize project structure with Vue, Vite, and Cypress setup

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+app.use(vuetify)
+
+app.mount('#app')
